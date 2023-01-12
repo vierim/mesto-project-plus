23f5f489d4e8e47421f35806c81d32cfaac163f5
web77code <web77@yandex.ru>
2023-01-12 12:00:21 +0300

    feat: add fake auth (temporary)

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,22 @@ import mongoose from "mongoose";
 import usersRouter from "./routes/users";
 import cardsRouter from "./routes/cards";
 
+import { ICustomRequest } from 'types';
+
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
+
+app.use((req: ICustomRequest, _res, next) => {
+  req.user = {
+    _id: "63bd2cae9b1fd0974185900f",
+  };
+
+  next();
+});
 
 app.use("/users", usersRouter);
 app.use("/cards", cardsRouter);
--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from "express";
 import Card from "../models/card";
 
+import { ICustomRequest } from "types";
+
 export const getCards = (_req: Request, res: Response) => {
   return Card.find({})
     .then((cards) => res.send({ cards }))
     .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };
 
-export const createCard = (req: Request, res: Response) => {
+export const createCard = (req: ICustomRequest, res: Response) => {
   const { name, link } = req.body;
+  const owner = req.user ? req.user._id : undefined;
 
-  return Card.create({ name, link })
+  return Card.create({ name, link, owner })
     .then((card) => res.send({ card }))
     .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
 };

91f7f381f99cec358f15e5721afc920f737077d1
web77code <web77@yandex.ru>
2023-01-12 12:38:30 +0300

    feat: add methods for updating user information and avatar

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { ICustomRequest } from "types";
 import User from "../models/user";
 
 export const getUsers = (_req: Request, res: Response) => {
@@ -21,4 +22,30 @@ export const createUser = (req: Request, res: Response) => {
   return User.create({ name, about, avatar })
     .then((user) => res.send({ user }))
     .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+export const updateProfile = (req: ICustomRequest, res: Response) => {
+  const { name, about } = req.body;
+
+  if (req.user) {
+    const id = req?.user._id;
+    return User.findByIdAndUpdate(
+      id,
+      { name, about },
+      { returnDocument: "after" }
+    )
+      .then((user) => res.send(user))
+      .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+  }
+};
+
+export const updateAvatar = (req: ICustomRequest, res: Response) => {
+  const { avatar } = req.body;
+
+  if (req.user) {
+    const id = req?.user._id;
+    return User.findByIdAndUpdate(id, { avatar }, { returnDocument: "after" })
+      .then((user) => res.send(user))
+      .catch(() => res.status(500).send({ message: "Произошла ошибка" }));
+  }
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser, getUserById, getUsers } from "../controllers/users";
+import { getUsers, getUserById, createUser, updateProfile, updateAvatar } from "../controllers/users";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.get("/:userId", getUserById);
 router.post("/", createUser);
+router.patch("/me", updateProfile);
+router.patch("/me/avatar", updateAvatar);
 
 export default router;

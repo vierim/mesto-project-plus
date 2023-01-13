d06213d47142b1506df564f102d6942d0ec93093
web77code <web77@yandex.ru>
2023-01-13 10:03:48 +0300

    refact: small code style corrections

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
-import { getUsers, getUserById, createUser, updateProfile, updateAvatar } from "../controllers/users";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateProfile,
+  updateAvatar,
+} from "../controllers/users";
 
 const router = Router();
 
 router.get("/", getUsers);
-router.get("/:userId", getUserById);
 router.post("/", createUser);
+
+router.get("/:userId", getUserById);
+
 router.patch("/me", updateProfile);
 router.patch("/me/avatar", updateAvatar);
 

296fba9eef5bf9e85701f60e309a43c4fc6cd372
web77code <web77@yandex.ru>
2023-01-19 10:37:19 +0300

    feat: add check for card owner in deleteCard controller

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -8,7 +8,7 @@ import STATUS_CODE from '../utils/constants';
 
 export const getCards = async (_req: Request, res: Response) => {
   try {
-    const cards = await Card.find({});
+    const cards = await Card.find({}).populate('owner', ['name', 'about']);
 
     return res.status(STATUS_CODE.OK).send(cards);
   } catch {
@@ -43,11 +43,19 @@ export const createCard = async (req: ICustomRequest, res: Response) => {
   }
 };
 
-export const deleteCard = async (req: Request, res: Response) => {
+export const deleteCard = async (req: ICustomRequest, res: Response) => {
   const { cardId } = req.params;
+  const userId = req.user?._id;
 
   try {
-    const card = await Card.findByIdAndDelete(cardId);
+    if (!userId) {
+      const error = new Error('Необходима авторизация');
+      error.name = 'AuthError';
+
+      throw error;
+    }
+
+    const card = await Card.findById(cardId).populate({ path: 'owner', match: { _id: userId } });
 
     if (!card) {
       const error = new Error('Карточка с таким id не найдена');
@@ -56,18 +64,62 @@ export const deleteCard = async (req: Request, res: Response) => {
       throw error;
     }
 
+    if (!card.owner) {
+      const error = new Error('Карточка с таким id принадлежит другому пользователю');
+      error.name = 'CardHasOtherOwner';
+
+      throw error;
+    }
+
+    await Card.findByIdAndDelete(cardId);
+
     return res.status(STATUS_CODE.OK).send({ message: 'Карточка удалена' });
   } catch (error) {
+    if (error instanceof Error && error.name === 'AuthError') {
+      return res
+        .status(STATUS_CODE.AUTH_ERROR)
+        .send({ message: 'Необходима авторизация' });
+    }
+
     if (error instanceof Error && error.name === 'CardNotFound') {
       return res
         .status(STATUS_CODE.NOT_FOUND)
         .send({ message: 'Карточка по указанному _id не найдена' });
     }
 
+    if (error instanceof Error && error.name === 'CardHasOtherOwner') {
+      return res
+        .status(STATUS_CODE.NOT_FOUND)
+        .send({ message: 'Карточка с таким id принадлежит другому пользователю' });
+    }
+
     return res
       .status(STATUS_CODE.DEFAULT_ERROR)
       .send({ message: 'На сервере произошла ошибка' });
   }
+
+  // try {
+  //   const card = await Card.findByIdAndDelete(cardId);
+
+  //   if (!card) {
+  //     const error = new Error('Карточка с таким id не найдена');
+  //     error.name = 'CardNotFound';
+
+  //     throw error;
+  //   }
+
+  //   return res.status(STATUS_CODE.OK).send({ message: 'Карточка удалена' });
+  // } catch (error) {
+  //   if (error instanceof Error && error.name === 'CardNotFound') {
+  //     return res
+  //       .status(STATUS_CODE.NOT_FOUND)
+  //       .send({ message: 'Карточка по указанному _id не найдена' });
+  //   }
+
+  //   return res
+  //     .status(STATUS_CODE.DEFAULT_ERROR)
+  //     .send({ message: 'На сервере произошла ошибка' });
+  // }
 };
 
 export const addLikeToCard = async (req: ICustomRequest, res: Response) => {
